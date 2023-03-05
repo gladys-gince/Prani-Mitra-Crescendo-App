662452a8b9ba716ae3244d425cf39fe4d0f84fd4
gladys-gince <gladys.gince@gmail.com>
2023-03-05 19:41:12 +0530

    first commit

---
+++ app/src/main/java/com/example/pranimitra/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.pranimitra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+{
+    private Button button;
+    private Button button1;
+    private Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button1 = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
